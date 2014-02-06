3fd1f18de47da05d8527282a738e5e5f32480c82
Steven Arcangeli <stevearc@stevearc.com>
2014-02-05 23:46:48 -0800

    Swap out salt for ansible

--- setup.sh
+++ setup.sh
@@ -1,27 +1,21 @@
 #!/bin/bash -ex
 
 main() {
-    local test_arg=
-    if [ "$1" == "test" ]; then
-        test_arg="test=True"
-    fi
-
     git submodule update --init --recursive > /dev/null
-    tar -zxf thin.tgz
-    local here=$(readlink -f .)
-    sed -i -e "s|<<file_root>>|$here/salt|" -e "s|<<home>>|$HOME|" -e "s|<<user>>|$USER|" config/minion
-    # Don't apt-get update if we're just testing. It takes too long
-    if [ ! "$test_arg" ]; then
-        sudo apt-get update -qq
-        sudo apt-get upgrade -y -qq
-    fi
+    sudo apt-get install -y sshpass
     if [ ! `which pip` ]; then
         sudo easy_install pip
     fi
-    sudo pip install msgpack-python jinja2 pyyaml
-    sudo python salt-call --local -l warning -c "$here/config" state.highstate $test_arg
-    sed -i -e "s|$here/salt|<<file_root>>|" -e "s|$HOME|<<home>>|" -e "s|$USER|<<user>>|" config/minion
-    git clean -fxd > /dev/null
+    if [ ! `which virtualenv` ]; then
+        sudo pip install virtualenv
+    fi
+    if [ ! -e 'venv' ]; then
+        virtualenv venv
+    fi
+    . venv/bin/activate
+    pip install ansible dopy
+
+    ansible-playbook -i inventory -v "$@"
 }
 
 main "$@"
