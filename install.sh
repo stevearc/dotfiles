e4414980f5265ccd8067c06dd108fbccb986dd7c
Steven Arcangeli <stevearc@stevearc.com>
2020-02-01 15:26:24 -0800

    Fix typo

--- install.sh
+++ install.sh
@@ -443,7 +443,7 @@ install-language-arduino() {
 
 install-language-js() {
   install-nvm
-  if !hascmd yarn; then
+  if ! hascmd yarn; then
     curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
     echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
     sudo apt-get update -qq
