86b20486cf381fde26457d825d7b195afabec671
Steven Arcangeli <stevearc@stevearc.com>
2020-11-12 11:01:22 -0800

    Nvim installer supports Mac

--- bash.d/10-install_neovim.sh
+++ bash.d/10-install_neovim.sh
@@ -1,33 +1,67 @@
 #!/bin/bash
 
+OSNAME=$(uname -s)
+if [ "$OSNAME" = "Darwin" ]; then
+  MAC=1
+fi
+
 install_neovim(){
-  local usage="install_neovim
+  local usage="install_neovim [VERSION]
 
 Options:
   -l, --list    List available versions
-  -i VERSION    Install Neovim version
   -s, --silent  Quiet download
 "
-  source parseargs.sh
-  parseargs "$usage" "$@" || return
+  LIST=
+  SILENT=
+  unset OPTIND
+  while getopts ":hsl-:" opt; do
+    case $opt in
+      -)
+        case $OPTARG in
+          help)
+            echo "$usage"
+            return 0
+            ;;
+          list)
+            LIST=1
+            ;;
+          silent)
+            SILENT=1
+            ;;
+          *)
+            echo "$usage"
+            return 1
+            ;;
+        esac
+        ;;
+      h)
+        echo "$usage"
+        return 0
+        ;;
+      s)
+        SILENT=1
+        ;;
+      l)
+        LIST=1
+        ;;
+      \?)
+        echo "$usage"
+        return 1
+        ;;
+    esac
+  done
+  shift $((OPTIND-1))
+  VERSION="$1"
+
   if [ -n "$LIST" ]; then
     curl -s https://api.github.com/repos/neovim/neovim/releases | jq ".[].tag_name" | tr -d '"'
   elif [ -n "$VERSION" ]; then
     echo "Installing NVIM $VERSION"
-    [ -n "$SILENT" ] && silent="-s"
-    curl $silent -L "https://github.com/neovim/neovim/releases/download/$VERSION/nvim.appimage" -o nvim.appimage
-    chmod +x nvim.appimage
-    if ! ./nvim.appimage --headless +qall 2> /dev/null; then
-      mkdir -p ~/.appimages
-      mv nvim.appimage ~/.appimages
-      cd ~/.appimages
-      ./nvim.appimage --appimage-extract > /dev/null
-      mv squashfs-root nvim-appimage
-      ln -s -f ~/.appimages/nvim-appimage/AppRun ~/bin/nvim
-      rm nvim.appimage
-      cd
+    if [ -n "$MAC" ]; then
+      _install_mac
     else
-      mv nvim.appimage ~/bin/nvim
+      _install_linux
     fi
     ~/bin/nvim --headless +UpdateRemotePlugins +qall
     echo -n "Installed "
@@ -37,4 +71,34 @@ Options:
     return 1
   fi
 }
+
+_install_mac() {
+  rm -rf .nvim-osx64
+  [ -n "$SILENT" ] && silent="-s"
+  curl $silent -LO https://github.com/neovim/neovim/releases/download/nightly/nvim-macos.tar.gz
+  tar xzf nvim-macos.tar.gz
+  rm -f nvim-macos.tar.gz
+  mv nvim-osx64 .nvim-osx64
+  rm -f ~/bin/nvim
+  ln -s ~/.nvim-osx64/bin/nvim ~/bin/nvim
+}
+
+_install_linux() {
+  [ -n "$SILENT" ] && silent="-s"
+  curl $silent -L "https://github.com/neovim/neovim/releases/download/$VERSION/nvim.appimage" -o nvim.appimage
+  chmod +x nvim.appimage
+  if ! ./nvim.appimage --headless +qall 2> /dev/null; then
+    mkdir -p ~/.appimages
+    mv nvim.appimage ~/.appimages
+    cd ~/.appimages
+    ./nvim.appimage --appimage-extract > /dev/null
+    mv squashfs-root nvim-appimage
+    ln -s -f ~/.appimages/nvim-appimage/AppRun ~/bin/nvim
+    rm nvim.appimage
+    cd
+  else
+    mv nvim.appimage ~/bin/nvim
+  fi
+}
+
 export install_neovim
