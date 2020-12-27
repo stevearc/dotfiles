9a64938df473dfd1d10fe4d4da12b18c27f86b65
Steven Arcangeli <stevearc@stevearc.com>
2020-12-26 17:48:21 -0800

    More gnome keyboard shortcuts

--- install.sh
+++ install.sh
@@ -572,6 +572,8 @@ setup-gnome() {
   gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-2 "['<Shift><Alt>at']"
   gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-3 "['<Shift><Alt>numbersign']"
   gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-4 "['<Shift><Alt>dollar']"
+  gsettings set org.gnome.desktop.wm.keybindings close "['<Alt>w']"
+  gsettings set org.gnome.desktop.wm.keybindings maximize "['<Primary>m']"
   gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed false
   gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 24
   gsettings set org.gnome.shell.extensions.dash-to-dock preferred-monitor 0
