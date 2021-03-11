e7ba3aace38a39366e8adcd2db4ab8a89a771485
Steven Arcangeli <stevearc@stevearc.com>
2021-03-10 18:49:09 -0800

    Upgrade WSL link

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@
 # Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
 # Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform
 # wsl --set-default-version 2
-# Start-Process -FilePath https://www.microsoft.com/en-us/p/ubuntu-1804-lts/9n9tngvndl3q
+# Start-Process -FilePath https://www.microsoft.com/en-us/p/ubuntu-2004-lts/9n6svws3rx71
 # Start-Process -FilePath https://www.microsoft.com/en-gb/p/windows-terminal/9n0dx20hk701
 # Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 set -e
