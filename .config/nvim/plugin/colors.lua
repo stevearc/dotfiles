54cd2a512afa66ec821ea16f83713038d54558f7
Steven Arcangeli <stevearc@stevearc.com>
2021-06-30 09:57:14 -0700

    Move barbar colors into tokyonight

--- .config/nvim/plugin/colors.lua
+++ .config/nvim/plugin/colors.lua
@@ -12,56 +12,16 @@ end
 
 -- Tokyo Night
 vim.g.tokyonight_style = "night"
+vim.g.tokyonight_dark_float = false
 vim.g.tokyonight_italic_comments = true
 vim.g.tokyonight_italic_keywords = false
 vim.g.tokyonight_sidebars = { "qf", "aerial", "terminal" }
-
-function stevearc:tokyonight()
-  local config = require("tokyonight.config")
-  local colors = require("tokyonight.colors").setup(config)
-  local util = require("tokyonight.util")
-  local barbar_theme = {
-    Current = {
-      base = { bg = colors.fg_gutter, fg = colors.fg },
-      Index = { bg = colors.fg_gutter, fg = colors.blue1 },
-      Mod = { bg = colors.fg_gutter, fg = colors.warning },
-      Sign = { bg = colors.fg_gutter, fg = colors.blue1 },
-      Target = { bg = colors.fg_gutter, fg = colors.red },
-    },
-    Visible = {
-      base = { bg = colors.none, fg = colors.fg },
-      Index = { bg = colors.none, fg = colors.blue1 },
-      Mod = { bg = colors.none, fg = colors.warning },
-      Sign = { bg = colors.none, fg = colors.blue1 },
-      Target = { bg = colors.none, fg = colors.red },
-    },
-    Inactive = {
-      base = { bg = colors.none, fg = colors.dark5 },
-      Index = { bg = colors.none, fg = colors.dark5 },
-      Mod = { bg = colors.none, fg = util.darken(colors.warning, 0.7) },
-      Sign = { bg = colors.none, fg = colors.border_highlight },
-      Target = { bg = colors.none, fg = colors.red },
-    },
-    Tabpages = {
-      base = { bg = colors.none, fg = colors.none },
-    },
-    Tabpage = {
-      Fill = { bg = colors.none, fg = colors.border_highlight },
-    },
-  }
-  for mode, pieces in pairs(barbar_theme) do
-    for piece, hl in pairs(pieces) do
-      local group = string.format("Buffer%s%s", mode, piece == "base" and "" or piece)
-      util.highlight(group, hl)
-    end
-  end
-end
 
 -- Solarized
 vim.g.solarized_extra_hi_groups = true
 vim.g.solarized_statusline = "flat"
 
-function stevearc:solarized8()
+function stevearc.solarized8()
   -- solarized8 is missing some colors for LSP
   vim.cmd([[hi link LspDiagnosticsDefaultError ALEError]])
   vim.cmd([[hi link LspDiagnosticsSignError ALEErrorSign]])
@@ -84,7 +44,6 @@ function stevearc:solarized8()
   vim.cmd([[hi ALEWarning gui=NONE cterm=NONE]])
 end
 
-vim.cmd([[autocmd ColorScheme tokyonight lua require'stevearc'.tokyonight()]])
 vim.cmd([[autocmd ColorScheme solarized8 lua require'stevearc'.solarized8()]])
 
 vim.cmd("colorscheme tokyonight")
