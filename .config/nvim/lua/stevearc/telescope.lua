26f7e95d0b8012ada20004c99f1fcd4e26cbd369
Steven Arcangeli <stevearc@stevearc.com>
2021-07-15 09:43:50 -0700

    Remove most of my telescope overrides

--- .config/nvim/lua/stevearc/telescope.lua
+++ .config/nvim/lua/stevearc/telescope.lua
@@ -1,99 +1,26 @@
-local themes = require('telescope.themes')
-local actions = require('telescope.actions')
-local path = require('telescope.path')
-local pickers = require('telescope.pickers')
-local finders = require('telescope.finders')
-local conf = require('telescope.config').values
+local themes = require("telescope.themes")
+local actions = require("telescope.actions")
+local pickers = require("telescope.pickers")
+local finders = require("telescope.finders")
+local conf = require("telescope.config").values
 
 local M = {}
 
-local function open_existing_or_new_tab(prompt_bufnr)
-  local entry = actions.get_selected_entry()
-  actions.close(prompt_bufnr)
-
-  if not entry then
-    print("[telescope] Nothing currently selected")
-    return
-  end
-
-  local filename = entry.path or entry.filename
-  filename = path.normalize(filename, vim.fn.getcwd())
-
-  for _,tabnum in ipairs(vim.api.nvim_list_tabpages()) do
-    local wins = vim.api.nvim_tabpage_list_wins(tabnum)
-    for _,winid in ipairs(wins) do
-      local bufnr = vim.api.nvim_win_get_buf(winid)
-      local bufname = path.normalize(vim.api.nvim_buf_get_name(bufnr), vim.fn.getcwd())
-      if bufname == filename or bufnr == entry.bufnr then
-        vim.api.nvim_set_current_tabpage(tabnum)
-        vim.api.nvim_set_current_win(winid)
-        return
-      end
-    end
-  end
-
-  if entry.bufnr then
-    vim.cmd(string.format(":tab sb %d", entry.bufnr))
-  else
-    vim.cmd(string.format(":tabedit %s", filename))
-    local bufnr = vim.api.nvim_get_current_buf()
-    vim.api.nvim_buf_set_option(bufnr, "buflisted", true)
-  end
-end
-
-M.find_files = function(opts)
-  opts = vim.tbl_extend('keep', opts or {}, {
-    attach_mappings = function(prompt_bufnr, map)
-      map('i', '<C-t>', open_existing_or_new_tab)
-      return true
-    end
-  })
-  -- Make the find command respect wildignore
-  if 1 == vim.fn.executable("rg") and vim.o.wildignore ~= "" then
-    opts.find_command = { 'rg', '--files'}
-    if opts.hidden then table.insert(opts.find_command, '--hidden') end
-    if opts.follow then table.insert(opts.find_command, '-L') end
-    for glob in string.gmatch(vim.o.wildignore, "[^,]+") do
-      table.insert(opts.find_command, '--iglob')
-      table.insert(opts.find_command, "!"..glob)
-    end
-    for _,glob in ipairs(opts.ignore or {}) do
-      table.insert(opts.find_command, '--iglob')
-      table.insert(opts.find_command, "!"..glob)
-    end
-  end
-  require('telescope.builtin').find_files(opts)
-end
-
 M.myles_find_files = function()
-  require('myles').find_files({
+  require("myles").find_files({
     previewer = false,
-    attach_mappings = function(prompt_bufnr, map)
-      map('i', '<C-t>', open_existing_or_new_tab)
-      return true
-    end
   })
-end
-
-M.buffers = function(opts)
-  opts = vim.tbl_extend('keep', opts or {}, {
-    attach_mappings = function(prompt_bufnr, map)
-      map('i', '<C-t>', open_existing_or_new_tab)
-      return true
-    end
-  })
-  require('telescope.builtin').buffers(opts)
 end
 
 M.select = function(title, items, callback)
-  local opts = themes.get_dropdown{
+  local opts = themes.get_dropdown({
     previewer = false,
-  }
+  })
   pickers.new(opts, {
     prompt_title = title,
-    finder = finders.new_table {
-      results = items
-    },
+    finder = finders.new_table({
+      results = items,
+    }),
     sorter = conf.generic_sorter(opts),
     attach_mappings = function(prompt_bufnr)
       actions.select_default:replace(function()
@@ -107,7 +34,7 @@ M.select = function(title, items, callback)
       end)
 
       return true
-    end
+    end,
   }):find()
 end
 
--- .config/nvim/plugin/telescope.vim
+++ .config/nvim/plugin/telescope.vim
@@ -2,14 +2,33 @@ lua <<EOF
 require('telescope').setup{
   defaults = {
     winblend = 10,
+    file_ignore_patterns = {
+      ".*%.png$",
+      ".*%.jpg$",
+      ".*%.jpeg$",
+      ".*%.gif$",
+      ".*%.wav$",
+      ".*%.aiff$",
+      ".*%.dll$",
+      ".*%.pdb$",
+      ".*%.mdb$",
+      ".*%.so$",
+      ".*%.swp$",
+      ".*%.zip$",
+      ".*%.gz$",
+      ".*%.bz2$",
+      ".*%.meta",
+      ".*%.cache",
+      ".*/%.git/"
+    },
   },
 }
 EOF
-nnoremap <leader>t <cmd>lua require('stevearc.telescope').find_files({previewer=false})<cr>
-nnoremap <leader>bb <cmd>lua require('stevearc.telescope').buffers({previewer=false})<cr>
+nnoremap <leader>t <cmd>lua require('telescope.builtin').find_files({previewer=false})<cr>
+nnoremap <leader>bb <cmd>lua require('telescope.builtin').buffers({previewer=false})<cr>
 nnoremap <leader>fg <cmd>lua require('telescope.builtin').live_grep()<cr>
 nnoremap <leader>fb <cmd>lua require('telescope.builtin').live_grep{grep_open_files = true}<cr>
 nnoremap <leader>fh <cmd>lua require('telescope.builtin').help_tags()<cr>
-nnoremap <leader>fd <cmd>lua require('stevearc.telescope').find_files({cwd='/home/stevearc/dotfiles/.config/nvim/', follow=true, hidden=true, previewer=false})<cr>
+nnoremap <leader>fd <cmd>lua require('telescope.builtin').find_files({cwd=string.format('%s/dotfiles/.config/nvim/', os.getenv('HOME')), follow=true, hidden=true, previewer=false})<cr>
 nnoremap <leader>fc <cmd>lua require('telescope.builtin').commands()<CR>
 nnoremap <leader>fs <cmd>lua require('telescope.builtin').lsp_dynamic_workspace_symbols()<CR>
