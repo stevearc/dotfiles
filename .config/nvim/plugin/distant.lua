4021b7d555082ffcc25d7acb69571eb73543823b
Steven Arcangeli <stevearc@stevearc.com>
2021-11-10 14:23:01 -0800

    Start experimenting with distant.nvim

---
+++ .config/nvim/plugin/distant.lua
@@ -0,0 +1,31 @@
+local actions = require("distant.nav.actions")
+
+-- TODO remaining features that I need:
+-- * cd into project dirs
+-- * telescope
+--   * find files
+--   * live grep
+-- * grep workflow (:grep and gw)
+-- * toggleterm
+-- * (optional) better dir view, closer to defx
+
+require("distant").setup({
+  ["*"] = {
+    mode = "ssh",
+    file = {
+      mappings = {
+        ["-"] = actions.up,
+      },
+    },
+    dir = {
+      mappings = {
+        ["<Return>"] = actions.edit,
+        ["-"] = actions.up,
+        ["d"] = actions.mkdir,
+        ["%"] = actions.newfile,
+        ["r"] = actions.rename,
+        ["D"] = actions.remove,
+      },
+    },
+  },
+})
