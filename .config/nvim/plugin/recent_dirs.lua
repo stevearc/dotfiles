97ec166232c83d6f4db0338a034d588fc2eaa062
Steven Arcangeli <stevearc@stevearc.com>
2021-11-19 13:16:49 -0800

    feat: project opener differentiates <cr> and <c-t>

--- .config/nvim/plugin/recent_dirs.lua
+++ .config/nvim/plugin/recent_dirs.lua
@@ -61,6 +61,23 @@ local function display_path(dir)
   return dir
 end
 
+local function open_project_tab(dir)
+  local has_any_bufs = false
+  local getopt = vim.api.nvim_buf_get_option
+  for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
+    if getopt(bufnr, "buflisted") and getopt(bufnr, "buftype") == "" and vim.api.nvim_buf_get_name(bufnr) ~= "" then
+      has_any_bufs = true
+      break
+    end
+  end
+  if has_any_bufs then
+    vim.cmd("tabnew")
+    vim.cmd("tcd " .. dir)
+  else
+    vim.cmd("cd " .. dir)
+  end
+end
+
 function stevearc.telescope_pick_project()
   local opts = themes.get_dropdown({
     previewer = false,
@@ -80,27 +97,24 @@ function stevearc.telescope_pick_project()
     }),
     sorter = conf.generic_sorter(opts),
     attach_mappings = function(prompt_bufnr, map)
+      actions.select_tab:replace(function()
+        local selection = state.get_selected_entry()
+        actions.close(prompt_bufnr)
+        open_project_tab(selection.value)
+      end)
       actions.select_default:replace(function()
         local selection = state.get_selected_entry()
         actions.close(prompt_bufnr)
-        local has_any_bufs = false
-        local getopt = vim.api.nvim_buf_get_option
-        for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
-          if
-            getopt(bufnr, "buflisted")
-            and getopt(bufnr, "buftype") == ""
-            and vim.api.nvim_buf_get_name(bufnr) ~= ""
-          then
-            has_any_bufs = true
-            break
+
+        for _, tabpage in ipairs(vim.api.nvim_list_tabpages()) do
+          local tabnr = vim.api.nvim_tabpage_get_number(tabpage)
+          local dir = vim.fn.getcwd(0, tabnr)
+          if dir == selection.value then
+            vim.api.nvim_set_current_tabpage(tabpage)
+            return
           end
         end
-        if has_any_bufs then
-          vim.cmd("tabnew")
-          vim.cmd("tcd " .. selection.value)
-        else
-          vim.cmd("cd " .. selection.value)
-        end
+        open_project_tab(selection.value)
       end)
 
       map("i", "<C-d>", function(prompt_bufnr)
