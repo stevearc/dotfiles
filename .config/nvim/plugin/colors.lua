46d34e670a65b2d646b43715ef55402f2016a729
Steven Arcangeli <stevearc@stevearc.com>
2021-12-03 13:09:20 -0800

    fix: no undercurls plz and thx

--- .config/nvim/plugin/colors.lua
+++ .config/nvim/plugin/colors.lua
@@ -17,6 +17,15 @@ function stevearc.tokyonight()
   local util = require("tokyonight.util")
   util.highlight("BufferTabpage", { bg = c.bg_statusline, fg = c.blue })
   util.highlight("BufferTabpageFill", { bg = c.bg_statusline, fg = c.none })
+  -- I HATE undercurls
+  util.highlight("DiagnosticUnderlineError", { style = "underline", sp = c.error })
+  util.highlight("DiagnosticUnderlineWarn", { style = "underline", sp = c.warn })
+  util.highlight("DiagnosticUnderlineInfo", { style = "underline", sp = c.info })
+  util.highlight("DiagnosticUnderlineHint", { style = "underline", sp = c.hint })
+  util.highlight("SpellBad", { sp = c.error, style = "underline" })
+  util.highlight("SpellCap", { sp = c.warning, style = "underline" })
+  util.highlight("SpellLocal", { sp = c.info, style = "underline" })
+  util.highlight("SpellRare", { sp = c.hint, style = "underline" })
 end
 vim.g.tokyonight_style = "night"
 vim.g.tokyonight_dark_float = false
