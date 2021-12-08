d2e316af1491b257704730fbcc26aff662e19546
Steven Arcangeli <stevearc@stevearc.com>
2021-12-08 11:55:35 -0800

    Drop support for neovim < 0.6

--- .config/nvim/plugin/lsp.lua
+++ .config/nvim/plugin/lsp.lua
@@ -1,75 +1,32 @@
 local stevearc = require("stevearc")
-local projects = require("projects")
-local GENERAL_DIAGNOSTICS = vim.diagnostic ~= nil
 
 -- vim.lsp.set_log_level("debug")
 
-if vim.diagnostic then
-  vim.diagnostic.config({
-    float = {
-      source = "always",
-    },
-    virtual_text = {
-      severity = { min = vim.diagnostic.severity.W },
-      source = "if_many",
-    },
-    severity_sort = true,
-  })
-end
+vim.diagnostic.config({
+  float = {
+    source = "always",
+  },
+  virtual_text = {
+    severity = { min = vim.diagnostic.severity.W },
+    source = "if_many",
+  },
+  severity_sort = true,
+})
 
 if vim.g.nerd_font then
-  -- Names changed in 0.6
-  if vim.diagnostic == nil then
-    vim.cmd([[
-      sign define LspDiagnosticsSignError text=   numhl=LspDiagnosticsSignError texthl=LspDiagnosticsSignError
-      sign define LspDiagnosticsSignWarning text=  numhl=LspDiagnosticsSignWarning texthl=LspDiagnosticsSignWarning
-      sign define LspDiagnosticsSignInformation text=• numhl=LspDiagnosticsSignInformation texthl=LspDiagnosticsSignInformation
-      sign define LspDiagnosticsSignHint text=• numhl=LspDiagnosticsSignHint texthl=LspDiagnosticsSignHint
-    ]])
-  else
-    vim.cmd([[
+  vim.cmd([[
       sign define DiagnosticSignError text=   numhl=DiagnosticSignError texthl=DiagnosticSignError
       sign define DiagnosticSignWarn text=  numhl=DiagnosticSignWarn texthl=DiagnosticSignWarn
       sign define DiagnosticSignInformation text=• numhl=DiagnosticSignInformation texthl=DiagnosticSignInformation
       sign define DiagnosticSignHint text=• numhl=DiagnosticSignHint texthl=DiagnosticSignHint
     ]])
-  end
 else
-  if vim.diagnostic == nil then
-    vim.cmd([[
-      sign define LspDiagnosticsSignError text=• numhl=LspDiagnosticsSignError texthl=LspDiagnosticsSignError
-      sign define LspDiagnosticsSignWarning text=• numhl=LspDiagnosticsSignWarning texthl=LspDiagnosticsSignWarning
-      sign define LspDiagnosticsSignInformation text=. numhl=LspDiagnosticsSignInformation texthl=LspDiagnosticsSignInformation
-      sign define LspDiagnosticsSignHint text=. numhl=LspDiagnosticsSignHint texthl=LspDiagnosticsSignHint
-    ]])
-  else
-    vim.cmd([[
+  vim.cmd([[
       sign define DiagnosticSignError text=• numhl=DiagnosticSignError texthl=DiagnosticSignError
       sign define DiagnosticSignWarn text=• numhl=DiagnosticSignWarn texthl=DiagnosticSignWarn
       sign define DiagnosticSignInformation text=. numhl=DiagnosticSignInformation texthl=DiagnosticSignInformation
       sign define DiagnosticSignHint text=. numhl=DiagnosticSignHint texthl=DiagnosticSignHint
     ]])
-  end
-end
-
--- callback args changed in Neovim 0.6. See:
--- https://github.com/neovim/neovim/pull/15504
-local function mk_handler(fn)
-  return function(...)
-    local config_or_client_id = select(4, ...)
-    local is_new = type(config_or_client_id) ~= "number"
-    if is_new then
-      fn(...)
-    else
-      local err = select(1, ...)
-      local method = select(2, ...)
-      local result = select(3, ...)
-      local client_id = select(4, ...)
-      local bufnr = select(5, ...)
-      local config = select(6, ...)
-      fn(err, result, { method = method, client_id = client_id, bufnr = bufnr }, config)
-    end
-  end
 end
 
 -- Make all the "jump" commands call zv after execution
@@ -91,15 +48,7 @@ end
 vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
 vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
 
-local function mapper(mode, key, result)
-  vim.api.nvim_buf_set_keymap(0, mode, key, result, { noremap = true, silent = true })
-end
-
-local _ft_config = {
-  ["_"] = {
-    code_action = true,
-    help = true,
-  },
+local ft_config = {
   vim = {
     help = false,
   },
@@ -110,39 +59,15 @@ local _ft_config = {
     code_action = false, -- TODO: this borks the omnisharp server
   },
 }
-local ft_config = setmetatable({}, {
-  __index = function(_, key)
-    if key == "javascriptreact" or key == "javascript.jsx" then
-      key = "javascript"
-    end
-    if key == "typescriptreact" or key == "typescript.tsx" then
-      key = "typescript"
-    end
-    local ret = _ft_config[key] or {}
-    return setmetatable(ret, {
-      __index = _ft_config["_"],
-    })
-  end,
-})
-
 local diagnostics_handler = vim.lsp.handlers["textDocument/publishDiagnostics"]
-vim.lsp.handlers["textDocument/publishDiagnostics"] = function(...)
-  local config_or_client_id = select(4, ...)
-  local is_new = type(config_or_client_id) ~= "number"
-  local client_id
-  if is_new then
-    local context = select(3, ...)
-    client_id = context.client_id
-  else
-    client_id = select(4, ...)
-  end
-  local client = vim.lsp.get_client_by_id(client_id)
+vim.lsp.handlers["textDocument/publishDiagnostics"] = function(err, result, context, config)
+  local client = vim.lsp.get_client_by_id(context.client_id)
   if client.config.diagnostics ~= false then
-    diagnostics_handler(...)
+    diagnostics_handler(err, result, context, config)
   end
 end
 
-vim.lsp.handlers["window/showMessage"] = mk_handler(function(_err, result, context, _config)
+vim.lsp.handlers["window/showMessage"] = function(_err, result, context, _config)
   local client_id = context.client_id
   local message_type = result.type
   local message = result.message
@@ -164,40 +89,24 @@ vim.lsp.handlers["window/showMessage"] = mk_handler(function(_err, result, conte
     vim.notify(string.format("LSP[%s] %s\n", client_name, message), map[message_type_name])
   end
   return result
-end)
+end
 
 function stevearc.on_update_diagnostics()
   local util = require("qf_helper.util")
   local config = require("qf_helper.config")
-  local total
-  if vim.diagnostic == nil then
-    local errors = vim.lsp.diagnostic.get_count(0, "Error")
-    local warnings = vim.lsp.diagnostic.get_count(0, "Warning")
-    total = errors + warnings
-  else
-    total = vim.tbl_count(vim.diagnostic.get(0, { severity = { min = vim.diagnostic.severity.W } }))
-  end
+  local total = vim.tbl_count(vim.diagnostic.get(0, { severity = { min = vim.diagnostic.severity.W } }))
   if total == 0 then
     vim.lsp.util.set_loclist({})
     if vim.fn.win_gettype() == "" then
       vim.cmd("silent! lclose")
     end
   else
-    if vim.diagnostic == nil then
-      vim.lsp.diagnostic.set_loclist({
-        open = false,
-        severity_limit = "Warning",
-        -- nvim 0.5
-        open_loclist = false,
-      })
-    else
-      vim.diagnostic.setloclist({
-        open = false,
-        severity = {
-          min = vim.diagnostic.severity.W,
-        },
-      })
-    end
+    vim.diagnostic.setloclist({
+      open = false,
+      severity = {
+        min = vim.diagnostic.severity.W,
+      },
+    })
     -- Resize the loclist
     if util.is_open("l") then
       local winid = vim.api.nvim_get_current_win()
@@ -223,11 +132,20 @@ local function adjust_formatting_capabilities(client, bufnr)
   end
 end
 
+vim.cmd([[augroup LSPDiagnostics
+  au!
+  autocmd DiagnosticChanged lua require'stevearc'.on_update_diagnostics()
+  augroup END]])
+
 local on_attach = function(client, bufnr)
-  local ft = vim.api.nvim_buf_get_option(0, "filetype")
+  local ft = vim.api.nvim_buf_get_option(bufnr, "filetype")
   local config = ft_config[ft] or {}
 
   adjust_formatting_capabilities(client, bufnr)
+
+  local function mapper(mode, key, result)
+    vim.api.nvim_buf_set_keymap(bufnr, mode, key, result, { noremap = true, silent = true })
+  end
 
   local function safemap(method, mode, key, result)
     if client.resolved_capabilities[method] then
@@ -235,16 +153,11 @@ local on_attach = function(client, bufnr)
     end
   end
 
-  local autocmd = GENERAL_DIAGNOSTICS and "DiagnosticsChanged" or "LspDiagnosticsChanged"
-  vim.cmd(string.format(
-    [[augroup LSPDiagnostics
-  au!
-  autocmd User %s lua require'stevearc'.on_update_diagnostics()
-  augroup END]],
-    autocmd
-  ))
-
-  vim.api.nvim_win_set_option(0, "signcolumn", "yes")
+  for _, winid in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
+    if vim.api.nvim_win_get_buf(winid) == bufnr then
+      vim.api.nvim_win_set_option(winid, "signcolumn", "yes")
+    end
+  end
 
   -- Standard LSP
   safemap("goto_definition", "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
@@ -252,13 +165,13 @@ local on_attach = function(client, bufnr)
   safemap("type_definition", "n", "gtd", "<cmd>lua vim.lsp.buf.type_definition()<CR>")
   safemap("implementation", "n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>")
   safemap("find_references", "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>")
-  if config.help then
+  if config.help ~= false then
     safemap("hover", "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>")
   end
   if client.resolved_capabilities.signature_help then
     mapper("i", "<c-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>")
   end
-  if config.code_action then
+  if config.code_action ~= false then
     mapper("n", "<leader>fa", "<cmd>lua vim.lsp.buf.code_action()<CR>")
     mapper("v", "<leader>fa", ":<C-U>lua vim.lsp.buf.range_code_action()<CR>")
   end
@@ -273,18 +186,14 @@ local on_attach = function(client, bufnr)
   safemap("document_range_formatting", "v", "=", "<cmd>lua vim.lsp.buf.range_formatting()<CR>")
   safemap("rename", "n", "<leader>r", "<cmd>lua vim.lsp.buf.rename()<CR>")
 
-  if vim.diagnostic == nil then
-    mapper("n", "<CR>", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics({border='rounded'})<CR>")
-  else
-    mapper("n", "<CR>", "<cmd>lua vim.diagnostic.open_float(0, {scope='line', border='rounded'})<CR>")
-  end
+  mapper("n", "<CR>", "<cmd>lua vim.diagnostic.open_float(0, {scope='line', border='rounded'})<CR>")
 
   if client.resolved_capabilities.document_highlight then
     vim.cmd([[autocmd CursorHold,CursorHoldI <buffer> lua vim.lsp.buf.document_highlight()]])
     vim.cmd([[autocmd CursorMoved,WinLeave <buffer> lua vim.lsp.buf.clear_references()]])
   end
 
-  vim.bo.omnifunc = "v:lua.vim.lsp.omnifunc"
+  vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
   require("lsp_signature").on_attach({}, bufnr)
   require("aerial").on_attach(client, bufnr)
@@ -297,11 +206,12 @@ capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
 local lspservers = {
   "bashls",
   "clangd",
+  "cssls",
   "gdscript",
   "gopls",
   "html",
-  "cssls",
   "omnisharp",
+  "rust_analyzer",
   "vimls",
 }
 for _, server in ipairs(lspservers) do
@@ -336,6 +246,7 @@ end
 require("lspconfig").pyright.setup({
   capabilities = capabilities,
   on_attach = on_attach,
+  -- pyright is real noisy when we're using sqlalchemy
   diagnostics = not is_using_sqlalchemy(),
 })
 require("lspconfig").jsonls.setup({
@@ -349,10 +260,6 @@ require("lspconfig").jsonls.setup({
   },
 })
 
-require("lspconfig").rust_analyzer.setup({
-  capabilities = capabilities,
-  on_attach = on_attach,
-})
 require("lspconfig").tsserver.setup({
   capabilities = capabilities,
   root_dir = function(fname)
@@ -368,6 +275,14 @@ require("lspconfig").tsserver.setup({
 })
 require("lspconfig").flow.setup({
   capabilities = capabilities,
+  root_dir = function(fname)
+    local util = require("lspconfig.util")
+    -- Disable flow when a typescript project is detected
+    if util.root_pattern("tsconfig.json")(fname) then
+      return nil
+    end
+    return util.root_pattern(".flowconfig")(fname)
+  end,
   on_attach = function(client, bufnr)
     require("flow").on_attach(client, bufnr)
     on_attach(client, bufnr)
@@ -382,8 +297,7 @@ require("lspconfig").flow.setup({
     },
   },
 })
-local home = vim.fn.expand("$HOME")
-local sumneko_root_path = home .. "/.local/share/nvim/language-servers/lua-language-server"
+local sumneko_root_path = os.getenv("HOME") .. "/.local/share/nvim/language-servers/lua-language-server"
 local sumneko_binary = sumneko_root_path .. "/bin/Linux/lua-language-server"
 require("lspconfig").sumneko_lua.setup({
   capabilities = capabilities,
@@ -400,8 +314,8 @@ require("lspconfig").sumneko_lua.setup({
       workspace = {
         -- Make the server aware of Neovim runtime files
         library = {
-          [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-          [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
+          [os.getenv("VIMRUNTIME") .. "/lua"] = true,
+          [os.getenv("VIMRUNTIME") .. "/lua/vim/lsp"] = true,
         },
       },
     },
@@ -426,9 +340,3 @@ require("lspconfig")["null-ls"].setup({
   end,
   on_attach = on_attach,
 })
-
--- Since we missed the FileType event when this runs on vim start, we should
--- manually make sure that LSP starts on the first file opened.
-vim.defer_fn(function()
-  vim.api.nvim_command("LspStart")
-end, 10)
