6286e2185fc5cac1ec2b487c6e4d583e35411f5f
Steven Arcangeli <stevearc@stevearc.com>
2022-01-02 17:30:22 -0800

    update aerial.nvim

--- .config/nvim/plugin/aerial.lua
+++ .config/nvim/plugin/aerial.lua
@@ -1,38 +1,29 @@
 local aerial = require("aerial")
-
-vim.g.aerial = {
+aerial.setup({
   default_direction = "prefer_left",
-  highlight_on_jump = 200,
+  highlight_on_jump = false,
   link_folds_to_tree = true,
   link_tree_to_folds = true,
   manage_folds = true,
   nerd_font = vim.g.nerd_font,
 
-  lsp = {
-    -- diagnostics_trigger_update = false,
-  },
-  -- filter_kind = {
-  --   ["_"] = { "Class" },
-  -- },
   backends = {
     ["_"] = { "treesitter", "lsp", "markdown" },
     -- ["_"] = { "treesitter", "markdown" },
     -- ["_"] = { "lsp", "markdown" },
   },
-  -- open_automatic = true,
-}
-
-aerial.register_attach_cb(function(bufnr)
-  local function map(mode, key, result)
-    vim.api.nvim_buf_set_keymap(bufnr, mode, key, result, { noremap = true, silent = true })
-  end
-  map("n", "<leader>a", "<cmd>AerialToggle!<CR>")
-  map("n", "{", "<cmd>AerialPrev<CR>")
-  map("v", "{", "<cmd>AerialPrev<CR>")
-  map("n", "}", "<cmd>AerialNext<CR>")
-  map("v", "}", "<cmd>AerialNext<CR>")
-  map("n", "[[", "<cmd>AerialPrevUp<CR>")
-  map("v", "[[", "<cmd>AerialPrevUp<CR>")
-  map("n", "]]", "<cmd>AerialNextUp<CR>")
-  map("v", "]]", "<cmd>AerialNextUp<CR>")
-end)
+  on_attach = function(bufnr)
+    local function map(mode, key, result)
+      vim.api.nvim_buf_set_keymap(bufnr, mode, key, result, { noremap = true, silent = true })
+    end
+    map("n", "<leader>a", "<cmd>AerialToggle!<CR>")
+    map("n", "{", "<cmd>AerialPrev<CR>")
+    map("v", "{", "<cmd>AerialPrev<CR>")
+    map("n", "}", "<cmd>AerialNext<CR>")
+    map("v", "}", "<cmd>AerialNext<CR>")
+    map("n", "[[", "<cmd>AerialPrevUp<CR>")
+    map("v", "[[", "<cmd>AerialPrevUp<CR>")
+    map("n", "]]", "<cmd>AerialNextUp<CR>")
+    map("v", "]]", "<cmd>AerialNextUp<CR>")
+  end,
+})
