17525129e0668ba6172ebb3aa3f87537299d8717
Steven Arcangeli <stevearc@stripe.com>
2022-04-01 14:58:43 -0700

    Improve hammerspoon window switcher

--- .config/hammerspoon/init.lua
+++ .config/hammerspoon/init.lua
@@ -1,3 +1,14 @@
+log = hs.logger.new('stevearc')
+log.setLogLevel(5)
+
+function dump(v)
+  local ret = hs.inspect.inspect(v)
+  log.d(ret)
+  return ret
+end
+
+sa = hs.alert.show
+
 -- Hot reload config
 function reloadConfig(files)
     doReload = false
@@ -13,15 +24,62 @@ end
 myWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
 hs.alert.show("Config loaded")
 
- -- include minimized/hidden windows, current Space only
-local switcher = hs.window.switcher.new(hs.window.filter.new():setCurrentSpace(true):setDefaultFilter{})
-hs.hotkey.bind('cmd','`',function()switcher:next()end)
-hs.hotkey.bind('cmd-shift','`',function()switcher:previous()end)
-
 --Predicate that checks if a window belongs to a screen
 function isInScreen(screen, win)
   return win:screen() == screen
 end
+
+ -- include minimized/hidden windows, current Space & screen only
+local switchers = setmetatable({}, {
+  __index = function(t, key)
+    local switcher = hs.window.switcher.new(hs.window.filter.new({}):setCurrentSpace(true):setScreens(key):setDefaultFilter({}))
+    rawset(t, key, switcher)
+    return switcher
+  end
+})
+
+hs.hotkey.bind('cmd','`',function()
+  local curwin = hs.window.focusedWindow()
+  local screen = curwin:screen()
+  local wins = hs.fnutils.filter(
+      hs.window.orderedWindows(),
+      hs.fnutils.partial(isInScreen, screen))
+  local idx
+  for i,win in ipairs(wins) do
+    if curwin == win then
+      idx = i
+      break
+    end
+  end
+  idx = (idx % #wins) + 1
+  wins[idx]:focus()
+  -- TODO switcher wasn't properly iterating windows on the laptop screen
+  -- local switcher = switchers[hs.window.focusedWindow():screen():name()]
+  -- switcher:next()
+end)
+
+hs.hotkey.bind('cmd-shift','`',function()
+  local curwin = hs.window.focusedWindow()
+  local screen = curwin:screen()
+  local wins = hs.fnutils.filter(
+      hs.window.orderedWindows(),
+      hs.fnutils.partial(isInScreen, screen))
+  local idx
+  for i,win in ipairs(wins) do
+    if curwin == win then
+      idx = i
+      break
+    end
+  end
+  idx = idx - 1
+  if idx == 0 then
+    idx = #wins
+  end
+  wins[idx]:focus()
+
+  -- local switcher = switchers[hs.window.focusedWindow():screen():name()]
+  -- switcher:previous()
+end)
 
 function focusScreen(screen)
   --Get windows within screen, ordered from front to back.
@@ -30,13 +88,22 @@ function focusScreen(screen)
   local windows = hs.fnutils.filter(
       hs.window.orderedWindows(),
       hs.fnutils.partial(isInScreen, screen))
+  
   local windowToFocus = #windows > 0 and windows[1] or hs.window.desktop()
   windowToFocus:focus()
 
   -- Move mouse to center of screen
   local pt = hs.geometry.rectMidPoint(screen:fullFrame())
-  hs.mouse.setAbsolutePosition(pt)
+  hs.mouse.absolutePosition(pt)
 end
+
+hs.hotkey.bind({'cmd', 'shift'}, 'd', function()
+  log.i("Debug")
+end)
+
+hs.hotkey.bind({'cmd', 'shift'}, 'c', function()
+  hs.toggleConsole()
+end)
 
 
 hs.hotkey.bind({'cmd'}, 'h', function()
