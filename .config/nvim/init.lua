c439acdafc49f34b8f2491e784f9354c1f078c20
Steven Arcangeli <stevearc@stevearc.com>
2022-05-27 18:16:20 -0700

    Tweak vim-matchup for better performance on large files

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -426,6 +426,10 @@ end
 
 -- vim-matchup
 vim.g.matchup_surround_enabled = 1
+vim.g.matchup_matchparen_nomode = "i"
+vim.g.matchup_matchparen_deferred = 1
+vim.g.matchup_matchparen_deferred_show_delay = 400
+vim.g.matchup_matchparen_deferred_hide_delay = 400
 vim.keymap.set({ "n", "x" }, "[", "<plug>(matchup-[%)")
 vim.keymap.set({ "n", "x" }, "]", "<plug>(matchup-]%)")
 
