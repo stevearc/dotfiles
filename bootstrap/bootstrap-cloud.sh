3366df4c2196548428b559024b24a6ec9b35fb81
Steven Arcangeli <stevearc@stevearc.com>
2022-07-15 16:26:58 -0700

    Tweaking cloud bootstrap script

--- bootstrap/bootstrap-cloud.sh
+++ bootstrap/bootstrap-cloud.sh
@@ -1,22 +1,24 @@
 #!/usr/bin/env bash
 # Bootstraps a basic Ubuntu cloud machine
-set -e
+set -ex
 
 apt update
-adduser stevearc || :
-adduser stevearc admin
+id -u stevearc 2>/dev/null || adduser --system --group --disabled-password --gecos "" --shell /bin/bash stevearc
+groups stevearc | grep admin || adduser stevearc admin
+echo "stevearc ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/10-stevearc-no-pass
 cp -r ~/.ssh /home/stevearc
 chown -R stevearc:stevearc /home/stevearc/.ssh
 apt install -y -q bsdmainutils git
 cd /home/stevearc
-su stevearc -c "git clone --recursive https://github.com/stevearc/dotfiles"
-cat << EOF >>.bashrc
+su stevearc -c "git clone --recursive -j 8 https://github.com/stevearc/dotfiles"
 echo "Running setup"
-cd ~/dotfiles
-rm ~/.bashrc
-./run dotfiles
-source ~/.bashrc
-./run install common
-./run install neovim
-./run language common
-EOF
+rm -f /home/stevearc/.bashrc
+if [ ! -e .bash_profile ]; then
+  su stevearc -c "echo '[ -e ~/.bashrc ] && source ~/.bashrc' > .bash_profile"
+  chown stevearc:stevearc .bash_profile
+fi
+su stevearc -l -c "cd ~/dotfiles && ./run dotfiles"
+su stevearc -l -c "cd ~/dotfiles && ./run install common"
+su stevearc -l -c "cd ~/dotfiles && ./run language common"
+su stevearc -l -c "cd ~/dotfiles && ./scripts/install_neovim.sh stable"
+su stevearc -l -c "cd ~/dotfiles && ./run install neovim"
