6f08411ed67ee87e1fed6087f7fc4065dba249d4
Steven Arcangeli <stevearc@stevearc.com>
2022-09-27 14:52:20 -0700

    Fix quickfix <CR> binding

--- .config/nvim/lua/quick_action.lua
+++ .config/nvim/lua/quick_action.lua
@@ -8,6 +8,8 @@ local M = {}
 ---@type table<string, QuickAction>
 local bindings = {}
 
+local autocmd_id
+
 ---@param lhs string
 local function run_action(lhs)
   local actions = vim.tbl_filter(function(action)
@@ -37,12 +39,25 @@ end
 M.add = function(lhs, action)
   lhs = lhs:lower()
   if not bindings[lhs] then
-    vim.keymap.set("n", lhs, function()
-      run_action(lhs)
-    end, { desc = "Action picker" })
     bindings[lhs] = {}
   end
   table.insert(bindings[lhs], action)
+  if not autocmd_id then
+    autocmd_id = vim.api.nvim_create_autocmd("FileType", {
+      desc = "Set up quick actions in normal buffers",
+      pattern = "*",
+      callback = function(params)
+        if vim.bo[params.buf].buftype ~= "" then
+          return
+        end
+        for lhs in pairs(bindings) do
+          vim.keymap.set("n", lhs, function()
+            run_action(lhs)
+          end, { buffer = params.buf, desc = "Action picker" })
+        end
+      end,
+    })
+  end
 end
 
 return M
