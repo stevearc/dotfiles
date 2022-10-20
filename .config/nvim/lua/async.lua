4ca66af1dda611fd61e578b4f2849ecf31035e06
Steven Arcangeli <stevearc@stevearc.com>
2022-10-20 11:54:58 -0700

    Add my lua multithread handler

---
+++ .config/nvim/lua/async.lua
@@ -0,0 +1,24 @@
+local M = {}
+
+local function create_async_handle(callback)
+  local async
+  async = vim.loop.new_async(vim.schedule_wrap(function(ret)
+    callback(unpack(vim.mpack.decode(ret)))
+    async:close()
+  end))
+  return async
+end
+
+M.run_in_thread = function(callback, fn, ...)
+  local handle = create_async_handle(callback)
+  vim.loop.new_thread(function(fn_str, async, args_str)
+    local args = vim.mpack.decode(args_str)
+    local function pack(...)
+      return { ... }
+    end
+    local ret = pack(loadstring(fn_str)(unpack(args)))
+    async:send(vim.mpack.encode(ret))
+  end, string.dump(fn), handle, vim.mpack.encode({ ... }))
+end
+
+return M
