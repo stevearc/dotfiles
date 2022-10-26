6ca0f282ba1e8e77ac4afc8ed2df69c925082467
Steven Arcangeli <stevearc@stevearc.com>
2022-10-25 22:08:30 -0700

    Improve colors

--- .config/nvim/plugin/colors.lua
+++ .config/nvim/plugin/colors.lua
@@ -14,6 +14,8 @@ local colorschemes = {
         all = {
           -- Make and/or/not stand out more
           ["@keyword.operator"] = { link = "@keyword" },
+          -- Make markdown links stand out
+          ["@text.reference"] = { link = "@keyword" },
         },
       },
     })
@@ -27,7 +29,7 @@ local colorschemes = {
         floats = "normal",
       },
       sidebars = vim.list_extend({ "qf", "help", "terminal" }, vim.g.sidebar_filetypes),
-      on_highlights = function(highlights)
+      on_highlights = function(highlights, c)
         for _, defn in pairs(highlights) do
           if defn.undercurl then
             defn.undercurl = false
@@ -36,6 +38,15 @@ local colorschemes = {
         end
         highlights.AerialLineNC = { link = "LspReferenceText" }
         highlights.OverseerOutput = { link = "NormalSB" }
+        highlights.TabLine = { fg = c.comment, bg = c.bg_statusline }
+        highlights.TabLineSel = { fg = c.bg, bg = c.blue }
+        highlights.TabLineModifiedSel = { fg = c.bg, bg = c.warning }
+        highlights.TabLineIndexSel = { fg = c.bg, bg = c.blue, bold = true }
+        highlights.TabLineIndexModifiedSel = { fg = c.bg, bg = c.warning, bold = true }
+        highlights.TabLineDivider = { fg = c.blue }
+        highlights.TabLineDividerSel = { fg = c.blue, bg = c.blue }
+        highlights.TabLineDividerVisible = { fg = c.blue }
+        highlights.TabLineDividerModifiedSel = { fg = c.warning, bg = c.warning }
       end,
     })
   end,
