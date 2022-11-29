77357e004921f2c8263e06c1e4627c4bb77033ef
Steven Arcangeli <stevearc@stevearc.com>
2022-11-29 12:52:11 -0800

    Start using nvim-treesitter-textobjects

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -35,11 +35,48 @@ safe_require("nvim-treesitter", function()
       enable = true,
       disable = should_disable,
     },
-    textsubjects = {
-      enable = true,
-      disable = should_disable,
-      keymaps = {
-        ["."] = "textsubjects-smart",
+    textobjects = {
+      select = {
+        enable = true,
+        disable = should_disable,
+        lookahead = true,
+        keymaps = {
+          ["af"] = "@function.outer",
+          ["if"] = "@function.inner",
+          ["ac"] = "@class.outer",
+          ["ic"] = "@class.inner",
+          ["aa"] = "@parameter.outer",
+          ["ia"] = "@parameter.inner",
+          ["ab"] = "@block.outer",
+          ["ib"] = "@block.inner",
+          ["al"] = "@loop.outer",
+          ["il"] = "@loop.inner",
+          ["ai"] = "@conditional.outer",
+          ["ii"] = "@conditional.inner",
+        },
+        include_surrounding_whitespace = false,
+      },
+      move = {
+        enable = true,
+        set_jumps = true,
+        goto_next_start = {
+          ["]f"] = "@function.outer",
+          ["]c"] = "@class.outer",
+          ["]a"] = "@parameter.inner",
+          ["]b"] = "@block.outer",
+          ["]l"] = "@loop.outer",
+          ["]i"] = "@conditional.outer",
+        },
+        goto_next_end = {},
+        goto_previous_start = {
+          ["[f"] = "@function.outer",
+          ["[c"] = "@class.outer",
+          ["[a"] = "@parameter.inner",
+          ["[b"] = "@block.outer",
+          ["[l"] = "@loop.outer",
+          ["[i"] = "@conditional.outer",
+        },
+        goto_previous_end = {},
       },
     },
   })
