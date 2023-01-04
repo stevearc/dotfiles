f3a2c2cf1014d9e73d92ecd6274b07dc7725fda5
Steven Arcangeli <stevearc@stevearc.com>
2023-01-04 00:06:09 -0800

    Move oil config into its own file

--- .config/nvim/lua/plugins/all.lua
+++ .config/nvim/lua/plugins/all.lua
@@ -214,78 +214,7 @@ lazy("gkeep.nvim", {
   end,
 })
 
-lazy.load("oil.nvim").require("oil", function(oil)
-  oil.setup({
-    trash = false,
-    skip_confirm_for_simple_edits = true,
-    restore_win_options = false,
-    keymaps = {
-      ["`"] = "actions.tcd",
-      ["~"] = "actions.cd",
-      ["gd"] = {
-        desc = "Toggle detail view",
-        callback = function()
-          local config = require("oil.config")
-          if #config.columns == 1 then
-            oil.set_columns({ "icon", "permissions", "size", "mtime" })
-          else
-            oil.set_columns({ "icon" })
-          end
-        end,
-      },
-    },
-  })
-  vim.keymap.set("n", "-", oil.open, { desc = "Open parent directory" })
-  vim.keymap.set("n", "_", function()
-    oil.open(vim.fn.getcwd())
-  end, { desc = "Open cwd" })
-  local function find_files()
-    local dir = oil.get_current_dir()
-    if vim.api.nvim_win_get_config(0).relative ~= "" then
-      vim.api.nvim_win_close(0, true)
-    end
-    stevearc.find_files({ cwd = dir, hidden = true })
-  end
-
-  local function livegrep()
-    local dir = oil.get_current_dir()
-    if vim.api.nvim_win_get_config(0).relative ~= "" then
-      vim.api.nvim_win_close(0, true)
-    end
-    require("telescope.builtin").live_grep({ cwd = dir })
-  end
-
-  ftplugin.set("oil", {
-    bindings = {
-      { "n", "<leader>ff", find_files, { desc = "[F]ind [F]iles in dir" } },
-      { "n", "<leader>fg", livegrep, { desc = "[F]ind by [G]rep in dir" } },
-    },
-    opt = {
-      conceallevel = 3,
-      concealcursor = "n",
-      list = false,
-      wrap = false,
-    },
-    callback = function(bufnr)
-      vim.api.nvim_buf_create_user_command(bufnr, "Save", function(params)
-        oil.save({ confirm = not params.bang })
-      end, {
-        desc = "Save oil changes with a preview",
-        bang = true,
-      })
-      vim.api.nvim_buf_create_user_command(bufnr, "EmptyTrash", function(params)
-        oil.empty_trash()
-      end, {
-        desc = "Empty the trash directory",
-      })
-      vim.api.nvim_buf_create_user_command(bufnr, "OpenTerminal", function(params)
-        require("oil.adapters.ssh").open_terminal()
-      end, {
-        desc = "Open the debug terminal for ssh connections",
-      })
-    end,
-  })
-end)
+lazy.load("oil.nvim").require("plugins.oil")
 
 lazy.load("aerial.nvim").require("plugins.aerial")
 lazy("lightspeed.nvim", {
@@ -387,7 +316,7 @@ lazy.require("overseer", function(overseer)
   vim.keymap.set("n", "<leader>od", "<cmd>OverseerQuickAction<CR>")
   vim.keymap.set("n", "<leader>os", "<cmd>OverseerTaskAction<CR>")
 end)
-lazy.load("nvim-hlslens").require("plugins/hlslens")
+lazy.load("nvim-hlslens").require("plugins.hlslens")
 
 lazy("nvim-FeMaco.lua", {
   commands = { "FeMaco" },
@@ -397,7 +326,7 @@ lazy("nvim-FeMaco.lua", {
   end,
 })
 
-require("plugins/ccc")
+require("plugins.ccc")
 
 lazy.require("quick_action", function(quick_action)
   quick_action.set_keymap("n", "<CR>", "menu")
@@ -414,7 +343,7 @@ lazy.require("quick_action", function(quick_action)
     end,
   })
 end)
-lazy.load("three.nvim").require("plugins/three")
+lazy.load("three.nvim").require("plugins.three")
 lazy("fidget.nvim", {
   req = "fidget",
   modules = { "^fidget" },
---
+++ .config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,85 @@
+local lazy = require("lazy")
+local ftplugin = lazy.require("ftplugin")
+
+lazy.require("oil", function(oil)
+  oil.setup({
+    trash = false,
+    skip_confirm_for_simple_edits = true,
+    restore_win_options = false,
+    keymaps = {
+      ["`"] = "actions.tcd",
+      ["~"] = "actions.cd",
+      ["gd"] = {
+        desc = "Toggle detail view",
+        callback = function()
+          local config = require("oil.config")
+          if #config.columns == 1 then
+            oil.set_columns({ "icon", "permissions", "size", "mtime" })
+          else
+            oil.set_columns({ "icon" })
+          end
+        end,
+      },
+    },
+  })
+  vim.keymap.set("n", "-", oil.open, { desc = "Open parent directory" })
+  vim.keymap.set("n", "_", function()
+    oil.open(vim.fn.getcwd())
+  end, { desc = "Open cwd" })
+  local function find_files()
+    local dir = oil.get_current_dir()
+    if vim.api.nvim_win_get_config(0).relative ~= "" then
+      vim.api.nvim_win_close(0, true)
+    end
+    stevearc.find_files({ cwd = dir, hidden = true })
+  end
+
+  local function livegrep()
+    local dir = oil.get_current_dir()
+    if vim.api.nvim_win_get_config(0).relative ~= "" then
+      vim.api.nvim_win_close(0, true)
+    end
+    require("telescope.builtin").live_grep({ cwd = dir })
+  end
+
+  ftplugin.set("oil", {
+    bindings = {
+      { "n", "<leader>ff", find_files, { desc = "[F]ind [F]iles in dir" } },
+      { "n", "<leader>fg", livegrep, { desc = "[F]ind by [G]rep in dir" } },
+    },
+    opt = {
+      conceallevel = 3,
+      concealcursor = "n",
+      list = false,
+      wrap = false,
+    },
+    callback = function(bufnr)
+      vim.api.nvim_buf_create_user_command(bufnr, "Save", function(params)
+        oil.save({ confirm = not params.bang })
+      end, {
+        desc = "Save oil changes with a preview",
+        bang = true,
+      })
+      vim.api.nvim_buf_create_user_command(bufnr, "EmptyTrash", function(params)
+        oil.empty_trash()
+      end, {
+        desc = "Empty the trash directory",
+      })
+      vim.api.nvim_buf_create_user_command(bufnr, "OpenTerminal", function(params)
+        require("oil.adapters.ssh").open_terminal()
+      end, {
+        desc = "Open the debug terminal for ssh connections",
+      })
+    end,
+  })
+
+  vim.api.nvim_create_autocmd("FileType", {
+    pattern = "oil_preview",
+    callback = function(args)
+      vim.keymap.set("n", "D", function()
+        vim.api.nvim_win_close(0, true)
+        oil.discard_all_changes()
+      end, { buffer = args.buf })
+    end,
+  })
+end)
