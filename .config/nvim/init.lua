4e86a7b5ceb79f75e5fa0ee42a058e6fab76ec82
Steven Arcangeli <stevearc@stevearc.com>
2023-01-18 08:54:00 -0800

    Disable netrw

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -388,7 +388,7 @@ require("lazy").setup({
         "gzip",
         "matchit",
         "matchparen",
-        -- "netrwPlugin",
+        "netrwPlugin",
         "tarPlugin",
         "tohtml",
         "tutor",
