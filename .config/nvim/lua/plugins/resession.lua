b60080b683eb15dc5787cc18b22ab800a5a706ab
Steven Arcangeli <stevearc@stevearc.com>
2023-02-01 22:04:12 -0800

    When saving sessions, don't save hidden buffers

--- .config/nvim/lua/plugins/resession.lua
+++ .config/nvim/lua/plugins/resession.lua
@@ -9,6 +9,7 @@ return {
   config = function()
     local resession = require("resession")
     local aug = vim.api.nvim_create_augroup("StevearcResession", {})
+    local visible_buffers = {}
     resession.setup({
       autosave = {
         enabled = true,
@@ -18,8 +19,23 @@ return {
         local dir = vim.fn.getcwd(-1, vim.api.nvim_tabpage_get_number(tabpage))
         return vim.startswith(vim.api.nvim_buf_get_name(bufnr), dir)
       end,
+      buf_filter = function(bufnr)
+        if not resession.default_buf_filter(bufnr) then
+          return false
+        end
+        return visible_buffers[bufnr] or require("three").is_buffer_in_any_tab(bufnr)
+      end,
       extensions = { aerial = {}, overseer = {}, quickfix = {}, three = {}, config_local = {} },
     })
+
+    resession.add_hook("pre_save", function()
+      visible_buffers = {}
+      for _, winid in ipairs(vim.api.nvim_list_wins()) do
+        if vim.api.nvim_win_is_valid(winid) then
+          visible_buffers[vim.api.nvim_win_get_buf(winid)] = winid
+        end
+      end
+    end)
 
     vim.keymap.set("n", "<leader>ss", resession.save, { desc = "[S]ession [S]ave" })
     vim.keymap.set("n", "<leader>st", function()
