ca8a138dca6271998d8f2749dd50160937d7a4cf
Steven Arcangeli <stevearc@stevearc.com>
2023-02-12 09:55:57 -0800

    More markdown concealers

--- .config/nvim/after/queries/markdown/highlights.scm
+++ .config/nvim/after/queries/markdown/highlights.scm
@@ -3,9 +3,17 @@
 ; Needs https://github.com/nvim-treesitter/nvim-treesitter/issues/4181
 ; (([(list_marker_star) (list_marker_minus)] @punctuation.special @conceal_star (#offset! @conceal_star 0 0 0 -1)) (#set! conceal "•"))
 
+; Checkbox list items
 ((task_list_marker_unchecked) @punctuation.special @conceal (#set! conceal ""))
 ((task_list_marker_checked) @punctuation.special @conceal (#set! conceal ""))
 
+; Use box drawing characters for tables
+(pipe_table_header ("|") @punctuation.special @conceal (#set! conceal "┃"))
+(pipe_table_delimiter_row ("|") @punctuation.special @conceal (#set! conceal "┃"))
+(pipe_table_delimiter_cell ("-") @punctuation.special @conceal (#set! conceal "━"))
+(pipe_table_row ("|") @punctuation.special @conceal (#set! conceal "┃"))
+
+; Block quotes
 ((block_quote_marker) @conceal (#set! conceal "▍"))
 ((block_quote
   (paragraph (inline
