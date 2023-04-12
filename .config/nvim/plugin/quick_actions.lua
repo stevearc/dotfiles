c1c71437be0063954bad854d5487fd835fb9cd05
Steven Arcangeli <stevearc@stevearc.com>
2023-04-11 21:09:21 -0700

    Quick menu <CR> action shows any level of diagnostic

--- .config/nvim/plugin/quick_actions.lua
+++ .config/nvim/plugin/quick_actions.lua
@@ -6,9 +6,7 @@ p.require("quick_action", function(quick_action)
     name = "Show diagnostics",
     condition = function()
       local lnum = vim.api.nvim_win_get_cursor(0)[1] - 1
-      return not vim.tbl_isempty(
-        vim.diagnostic.get(0, { lnum = lnum, severity = { min = vim.diagnostic.severity.WARN } })
-      )
+      return not vim.tbl_isempty(vim.diagnostic.get(0, { lnum = lnum }))
     end,
     action = function()
       vim.diagnostic.open_float(0, { scope = "line", border = "rounded" })
