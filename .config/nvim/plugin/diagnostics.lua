edeb15c9562767b7065f91234a4339d4d0cb110f
Steven Arcangeli <stevearc@stevearc.com>
2023-04-11 21:09:05 -0700

    Fix diagnostics info sign

--- .config/nvim/plugin/diagnostics.lua
+++ .config/nvim/plugin/diagnostics.lua
@@ -15,14 +15,14 @@ if vim.g.nerd_font then
   vim.cmd([[
       sign define DiagnosticSignError text=󰅚   numhl=DiagnosticSignError texthl=DiagnosticSignError
       sign define DiagnosticSignWarn text=󰀪  numhl=DiagnosticSignWarn texthl=DiagnosticSignWarn
-      sign define DiagnosticSignInformation text=• numhl=DiagnosticSignInformation texthl=DiagnosticSignInformation
+      sign define DiagnosticSignInfo text=• numhl=DiagnosticSignInfo texthl=DiagnosticSignInfo
       sign define DiagnosticSignHint text=• numhl=DiagnosticSignHint texthl=DiagnosticSignHint
     ]])
 else
   vim.cmd([[
       sign define DiagnosticSignError text=• numhl=DiagnosticSignError texthl=DiagnosticSignError
       sign define DiagnosticSignWarn text=• numhl=DiagnosticSignWarn texthl=DiagnosticSignWarn
-      sign define DiagnosticSignInformation text=. numhl=DiagnosticSignInformation texthl=DiagnosticSignInformation
+      sign define DiagnosticSignInfo text=. numhl=DiagnosticSignInfo texthl=DiagnosticSignInfo
       sign define DiagnosticSignHint text=. numhl=DiagnosticSignHint texthl=DiagnosticSignHint
     ]])
 end
