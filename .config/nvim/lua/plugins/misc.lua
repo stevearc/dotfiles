23e3c878176dd9b821deb7b490e5f05498fe134a
Steven Arcangeli <stevearc@stevearc.com>
2023-04-24 10:49:24 -0700

    Trying out copilot

--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -102,4 +102,35 @@ return {
       end,
     },
   },
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    opts = {
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        debounce = 75,
+        keymap = {
+          accept = "<C-y>",
+          accept_word = false,
+          accept_line = false,
+          next = "<C-n>",
+          prev = "<C-p>",
+          dismiss = "<C-e>",
+        },
+      },
+      panel = {
+        enabled = true,
+        auto_refresh = true,
+        keymap = {
+          jump_prev = "[[",
+          jump_next = "]]",
+          accept = "<C-y>",
+          refresh = "gr",
+          open = "<M-CR>",
+        },
+      },
+    },
+  },
 }
