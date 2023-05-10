8840cb9272324b74aa1e903a2a9215c0a73461c1
Steven Arcangeli <stevearc@stevearc.com>
2023-05-10 00:37:05 -0700

    Binding to toggle overseer bottom dock

--- .config/nvim/lua/plugins/overseer.lua
+++ .config/nvim/lua/plugins/overseer.lua
@@ -17,7 +17,7 @@ return {
     { "<leader>or", "<cmd>OverseerRun<CR>", mode = "n" },
     { "<leader>oc", "<cmd>OverseerRunCmd<CR>", mode = "n" },
     { "<leader>ol", "<cmd>OverseerLoadBundle<CR>", mode = "n" },
-    { "<leader>ob", "<cmd>OverseerBuild<CR>", mode = "n" },
+    { "<leader>ob", "<cmd>OverseerToggle! bottom<CR>", mode = "n" },
     { "<leader>od", "<cmd>OverseerQuickAction<CR>", mode = "n" },
     { "<leader>os", "<cmd>OverseerTaskAction<CR>", mode = "n" },
   },
