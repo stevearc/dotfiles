01c27c93b29e95892bd30ae913825348bc68b033
Steven Arcangeli <stevearc@stevearc.com>
2023-05-24 08:32:38 -0700

    Neorg doesn't need a special compiler on mac anymore

--- .config/nvim/lua/plugins/neorg.lua
+++ .config/nvim/lua/plugins/neorg.lua
@@ -1,22 +1,6 @@
--- Treesitter parser requires a compiler with C++14 support
--- On mac, you can get this with `brew install gcc`
-local enabled = true
-if vim.loop.os_uname().sysname == "Darwin" then
-  enabled = false
-  local bins = vim.split(vim.fn.glob("/opt/homebrew/Cellar/gcc/*/bin/gcc-*"), "\n")
-  for _, bin in ipairs(bins) do
-    local basename = vim.fn.fnamemodify(bin, ":t")
-    if basename:match("^gcc%-%d+$") then
-      vim.env.CC = bin
-      enabled = true
-      break
-    end
-  end
-end
 return {
   "nvim-neorg/neorg",
   dependencies = { "nvim-lua/plenary.nvim", "nvim-treesitter/nvim-treesitter", "jbyuki/venn.nvim" },
-  enabled = enabled,
   build = ":Neorg sync-parsers",
   keys = {
     { "<CR>", ":VBox<CR>", mode = "v" },
