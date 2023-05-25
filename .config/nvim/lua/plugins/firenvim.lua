b2b58f48b74912ec842608b7f7fab9386af786b9
Steven Arcangeli <stevearc@stevearc.com>
2023-05-24 19:52:09 -0700

    Fix firenvim build step

--- .config/nvim/lua/plugins/firenvim.lua
+++ .config/nvim/lua/plugins/firenvim.lua
@@ -1,7 +1,9 @@
 return {
   "glacambre/firenvim",
   lazy = not vim.g.started_by_firenvim,
-  build = "call firenvim#install(0)",
+  build = function()
+    vim.fn["firenvim#install"](0)
+  end,
   config = function()
     if not vim.g.started_by_firenvim then
       return
