4908121d1c76d5b583441b3d9021166ec6d26865
Steven Arcangeli <stevearc@stevearc.com>
2023-07-13 18:36:25 -0700

    Extract image.nvim config into separate file

--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -1,5 +1,3 @@
-local uv = vim.uv or vim.loop
-local is_mac = uv.os_uname().sysname == "Darwin"
 return {
   { "stevearc/stickybuf.nvim", cmd = { "PinBuffer", "PinBuftype", "PinFiletype" }, opts = {} },
   { "lambdalisue/suda.vim", cmd = { "SudaRead", "SudaWrite" } },
@@ -149,22 +147,5 @@ return {
         enabled = false,
       },
     },
-  },
-  {
-    "3rd/image.nvim",
-    build = function()
-      local has_magick = pcall(require, "magick")
-      if not has_magick and vim.fn.executable("luarocks") == 1 then
-        if is_mac then
-          vim.fn.system("luarocks --lua-dir=$(brew --prefix)/opt/lua@5.1 --lua-version=5.1 install magick")
-        else
-          vim.fn.system("luarocks --local --lua-version=5.1 install magick")
-        end
-        if vim.v.shell_error ~= 0 then
-          vim.notify("Error installing magick with luarocks", vim.log.levels.WARN)
-        end
-      end
-    end,
-    opts = {},
   },
 }
