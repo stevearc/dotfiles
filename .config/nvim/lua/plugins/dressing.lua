0b2ab4fedf0ede75884fb36e38095f31a53201bb
Steven Arcangeli <stevearc@stevearc.com>
2023-09-28 12:00:06 -0700

    Shim for vim.ui.open

--- .config/nvim/lua/plugins/dressing.lua
+++ .config/nvim/lua/plugins/dressing.lua
@@ -35,4 +35,47 @@ return {
       )
     end)
   end,
+  init = function()
+    -- Shim for vim.ui.open
+    if vim.ui.open then
+      return
+    end
+
+    ---Copied from vim.ui.open in Neovim 0.10+
+    ---@param path string
+    ---@return nil|string[] cmd
+    ---@return nil|string error
+    local function get_open_cmd(path)
+      if vim.fn.has("mac") == 1 then
+        return { "open", path }
+      elseif vim.fn.has("win32") == 1 then
+        if vim.fn.executable("rundll32") == 1 then
+          return { "rundll32", "url.dll,FileProtocolHandler", path }
+        else
+          return nil, "rundll32 not found"
+        end
+      elseif vim.fn.executable("wslview") == 1 then
+        return { "wslview", path }
+      elseif vim.fn.executable("xdg-open") == 1 then
+        return { "xdg-open", path }
+      else
+        return nil, "no handler found"
+      end
+    end
+
+    vim.ui.open = function(path)
+      local is_uri = path:match("%w+:")
+      if not is_uri then
+        path = vim.fn.expand(path)
+      end
+      local cmd, err = get_open_cmd(path)
+      if not cmd or err then
+        return nil, err
+      end
+      local jid = vim.fn.jobstart(cmd, { detach = true })
+      if jid <= 0 then
+        return nil, "Failed to start job"
+      end
+    end
+  end,
 }
