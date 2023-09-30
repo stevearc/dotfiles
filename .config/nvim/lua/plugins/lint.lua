04d4a05632e5b3e49d741be80a500f6d594ee1f3
Steven Arcangeli <stevearc@stevearc.com>
2023-09-29 21:55:32 -0700

    Tweak nvim-lint

--- .config/nvim/lua/plugins/lint.lua
+++ .config/nvim/lua/plugins/lint.lua
@@ -3,14 +3,12 @@ return {
   "mfussenegger/nvim-lint",
   ft = {
     "javascript",
-    "javascript.jsx",
     "javascriptreact",
     "lua",
     "python",
     "rst",
     "sh",
     "typescript",
-    "typescript.tsx",
     "typescriptreact",
     "vim",
     "yaml",
@@ -18,14 +16,12 @@ return {
   opts = {
     linters_by_ft = {
       javascript = { "eslint_d" },
-      ["javascript.jsx"] = { "eslint_d" },
       javascriptreact = { "eslint_d" },
       lua = { "luacheck" },
       python = { "mypy", "pylint" },
       rst = { "rstlint" },
       sh = { "shellcheck" },
       typescript = { "eslint_d" },
-      ["typescript.tsx"] = { "eslint_d" },
       typescriptreact = { "eslint_d" },
       vim = { "vint" },
       yaml = { "yamllint" },
@@ -41,7 +37,7 @@ return {
     local timer = assert(uv.new_timer())
     local DEBOUNCE_MS = 500
     local aug = vim.api.nvim_create_augroup("Lint", { clear = true })
-    vim.api.nvim_create_autocmd({ "BufWritePost", "TextChanged", "InsertLeave" }, {
+    vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "TextChanged", "InsertLeave" }, {
       group = aug,
       callback = function()
         local bufnr = vim.api.nvim_get_current_buf()
