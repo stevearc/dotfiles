cf17e8c5119de069ce85fbdbb5d071c36cd775aa
Steven Arcangeli <stevearc@stevearc.com>
2024-03-17 22:34:40 -0700

    Lazy load typescript-tools

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -219,6 +219,7 @@ return {
   },
   {
     "pmizio/typescript-tools.nvim",
+    ft = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
     dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
     opts = {
       root_dir = function(fname)
