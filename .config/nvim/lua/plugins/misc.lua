88534af43692f7873a51e324ba967d540736adb1
Steven Arcangeli <stevearc@stevearc.com>
2024-04-19 13:26:21 -0400

    Fix flatten config

--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -104,26 +104,27 @@ return {
         gitcommit = true,
         gitrebase = true,
       },
-      post_open = function(bufnr, winnr, ft, is_blocking)
-        vim.w[winnr].is_remote = true
-        if is_blocking then
-          vim.bo[bufnr].bufhidden = "wipe"
-          local has_stickybuf, stickybuf = pcall(require, "stickybuf")
-          if has_stickybuf then
-            stickybuf.pin(winnr)
+      callbacks = {
+        post_open = function(bufnr, winnr, ft, is_blocking)
+          if is_blocking then
+            vim.bo[bufnr].bufhidden = "wipe"
+            local has_stickybuf, stickybuf = pcall(require, "stickybuf")
+            if has_stickybuf then
+              stickybuf.pin(winnr)
+            end
+            vim.api.nvim_create_autocmd("BufHidden", {
+              desc = "Close window when buffer is hidden",
+              callback = function()
+                if vim.api.nvim_win_is_valid(winnr) then
+                  vim.api.nvim_win_close(winnr, true)
+                end
+              end,
+              buffer = bufnr,
+              once = true,
+            })
           end
-          vim.api.nvim_create_autocmd("BufHidden", {
-            desc = "Close window when buffer is hidden",
-            callback = function()
-              if vim.api.nvim_win_is_valid(winnr) then
-                vim.api.nvim_win_close(winnr, true)
-              end
-            end,
-            buffer = bufnr,
-            once = true,
-          })
-        end
-      end,
+        end,
+      },
     },
   },
   {
