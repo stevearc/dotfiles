3e18b10bc877cf7cddf5951409caa1d8489e83cc
Steven Arcangeli <stevearc@stevearc.com>
2024-07-22 20:48:55 -0700

    Nightfox tweaks

--- .config/nvim/lua/plugins/nightfox.lua
+++ .config/nvim/lua/plugins/nightfox.lua
@@ -5,17 +5,15 @@ return {
   opts = {
     groups = {
       all = {
+        ["@markup.italic"] = { style = "italic" },
         -- Make and/or/not stand out more
         ["@keyword.operator"] = { link = "@keyword" },
-        -- Make markdown links stand out
+        -- -- Make markdown links stand out
         ["@text.reference"] = { link = "@keyword" },
-        ["@text.emphasis"] = { style = "italic" },
-        ["@text.strong"] = { style = "bold" },
         ["@text.literal"] = { style = "" }, -- Don't italicize
         ["@codeblock"] = { bg = "palette.bg0" },
         ["@neorg.markup.strikethrough"] = { fg = "palette.comment", style = "strikethrough" },
       },
     },
   },
-  config = true,
 }
