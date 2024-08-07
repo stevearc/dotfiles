0da3764e413627ec8b9e7978c23c49ead8fd53c6
Steven Arcangeli <stevearc@stevearc.com>
2024-08-06 22:03:52 -0700

    keymap to save line to quickfix for debugging

--- .config/nvim/lua/plugins/quickfix.lua
+++ .config/nvim/lua/plugins/quickfix.lua
@@ -55,6 +55,21 @@ return {
         function() require("quicker").toggle({ loclist = true }) end,
         desc = "Toggle [L]oclist",
       },
+      {
+        "<leader>Q",
+        function()
+          vim.fn.setqflist({}, "a", {
+            items = {
+              {
+                bufnr = vim.api.nvim_get_current_buf(),
+                lnum = vim.api.nvim_win_get_cursor(0)[1],
+                text = vim.api.nvim_get_current_line(),
+              },
+            },
+          })
+        end,
+        desc = "Add to [Q]uickfix",
+      },
     },
   },
 }
