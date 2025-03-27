3ab8c74bdd73d12028dcdd3d5f8242449e017e48
Steven Arcangeli <stevearc@stevearc.com>
2025-03-26 17:51:07 -0700

    update remote host display

--- bin/tmux-show-host.sh
+++ bin/tmux-show-host.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
-if [ -e /pay/conf/mydev-remote-name ]; then
-  cat /pay/conf/mydev-remote-name
+if [ -e /pay/conf/box-name ]; then
+  cat /pay/conf/box-name
 else
   hostname | cut -f 1 -d .
 fi
