bb8c370a98de7f4e48e1053682b95fcd1fe5caab
Steven Arcangeli <stevearc@stevearc.com>
2025-03-28 09:29:39 -0700

    update diagnostics config for nvim-0.11

--- .config/nvim/plugin/diagnostics.lua
+++ .config/nvim/plugin/diagnostics.lua
@@ -10,38 +10,67 @@ local icons = vim.g.nerd_font and {
   Hint = ".",
 }
 
-vim.diagnostic.config({
-  float = {
-    source = true,
-    border = "rounded",
+if vim.fn.has("nvim-0.11") == 1 then
+  vim.diagnostic.config({
+    float = {
+      source = true,
+      severity_sort = true,
+    },
+    jump = {
+      severity = { min = vim.diagnostic.severity.W },
+    },
+    underline = false,
+    virtual_lines = {
+      current_line = true,
+    },
     severity_sort = true,
-  },
-  virtual_text = {
-    severity = { min = vim.diagnostic.severity.W },
-    source = "if_many",
-  },
-  severity_sort = true,
-  signs = {
-    text = {
-      [vim.diagnostic.severity.ERROR] = icons.Error,
-      [vim.diagnostic.severity.WARN] = icons.Warn,
-      [vim.diagnostic.severity.INFO] = icons.Info,
-      [vim.diagnostic.severity.HINT] = icons.Hint,
+    signs = {
+      text = {
+        [vim.diagnostic.severity.ERROR] = icons.Error,
+        [vim.diagnostic.severity.WARN] = icons.Warn,
+        [vim.diagnostic.severity.INFO] = icons.Info,
+        [vim.diagnostic.severity.HINT] = icons.Hint,
+      },
+      numhl = {
+        [vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
+        [vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
+      },
     },
-    numhl = {
-      [vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
-      [vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
+  })
+else
+  vim.diagnostic.config({
+    float = {
+      source = true,
+      border = "rounded",
+      severity_sort = true,
     },
-  },
-})
+    virtual_text = {
+      severity = { min = vim.diagnostic.severity.W },
+      source = "if_many",
+    },
+    severity_sort = true,
+    signs = {
+      text = {
+        [vim.diagnostic.severity.ERROR] = icons.Error,
+        [vim.diagnostic.severity.WARN] = icons.Warn,
+        [vim.diagnostic.severity.INFO] = icons.Info,
+        [vim.diagnostic.severity.HINT] = icons.Hint,
+      },
+      numhl = {
+        [vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
+        [vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
+      },
+    },
+  })
 
-vim.keymap.set(
-  "n",
-  "[d",
-  function() vim.diagnostic.goto_prev({ severity = { min = vim.diagnostic.severity.WARN } }) end
-)
-vim.keymap.set(
-  "n",
-  "]d",
-  function() vim.diagnostic.goto_next({ severity = { min = vim.diagnostic.severity.WARN } }) end
-)
+  vim.keymap.set(
+    "n",
+    "[d",
+    function() vim.diagnostic.goto_prev({ severity = { min = vim.diagnostic.severity.WARN } }) end
+  )
+  vim.keymap.set(
+    "n",
+    "]d",
+    function() vim.diagnostic.goto_next({ severity = { min = vim.diagnostic.severity.WARN } }) end
+  )
+end
