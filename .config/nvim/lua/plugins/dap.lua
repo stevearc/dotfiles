427dd5fd4fb56d2282bfb26f3092c0925ac0d602
Steven Arcangeli <stevearc@stevearc.com>
2025-05-30 17:09:12 -0700

    Customize dapui layout

--- .config/nvim/lua/plugins/dap.lua
+++ .config/nvim/lua/plugins/dap.lua
@@ -39,7 +39,33 @@ return {
       local dapui = require("dapui")
       local dap = require("dap")
       -- dap.set_log_level("DEBUG")
-      dapui.setup()
+      dapui.setup({
+        layouts = {
+          {
+            elements = {
+              -- Provide IDs as strings or tables with "id" and "size" keys
+              {
+                id = "scopes",
+                size = 0.25,
+              },
+              { id = "breakpoints", size = 0.25 },
+              { id = "stacks", size = 0.25 },
+              { id = "watches", size = 0.25 },
+            },
+            size = 40,
+            position = "left",
+          },
+          {
+            elements = {
+              "repl",
+              -- TODO need to figure out how to make this play nice with window resizing
+              -- "console",
+            },
+            size = 10,
+            position = "bottom",
+          },
+        },
+      })
       dap.listeners.after.event_initialized["dapui_config"] = function() dapui.open() end
       dap.listeners.before.event_terminated["dapui_config"] = function() dapui.close() end
       dap.listeners.before.event_exited["dapui_config"] = function() dapui.close() end
