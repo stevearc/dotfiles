f11eb0ea173bfbd06074b08cd495a30b1f8e1b95
Steven Arcangeli <stevearc@stevearc.com>
2026-03-23 10:08:01 -0700

    more devbox tweaks

--- bootstrap/bootstrap-cloud.sh
+++ bootstrap/bootstrap-cloud.sh
@@ -5,7 +5,7 @@ set -ex
 apt update
 id -u stevearc 2>/dev/null || adduser --system --group --disabled-password --gecos "" --shell /bin/bash stevearc
 groups stevearc | grep admin || adduser stevearc admin
-echo "stevearc ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/10-stevearc-no-pass
+echo "stevearc ALL=(ALL) NOPASSWD:ALL" >/etc/sudoers.d/10-stevearc-no-pass
 cp -r ~/.ssh /home/stevearc
 chown -R stevearc:stevearc /home/stevearc/.ssh
 apt install -y -q bsdmainutils git
@@ -22,3 +22,4 @@ su stevearc -l -c "cd ~/dotfiles && ./run install common"
 su stevearc -l -c "cd ~/dotfiles && ./scripts/install_neovim.sh stable"
 su stevearc -l -c "cd ~/dotfiles && ./run install neovim"
 su stevearc -l -c "cd ~/dotfiles && ./run language common"
+echo "Done!"
